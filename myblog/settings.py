f3ed79f54be023df644c5f6e3e5fce6f4775ecce
spoiledPiggy <hitzhuwen@gmail.com>
2015-06-25 14:31:24 -0400

    add bootstrap template to the website

--- myblog/settings.py
+++ myblog/settings.py
@@ -84,6 +84,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+#
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# if DEBUG:
+#     MEDIA_URL = '/media/'
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static-only')
+#     MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+#     STATICFILES_DIRS = (
+#         os.path.join(BASE_DIR, 'static', 'static'), )
